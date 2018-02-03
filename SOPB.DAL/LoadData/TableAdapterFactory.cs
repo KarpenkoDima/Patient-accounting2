51d1c957a14c153b176ff75291a6a9463493ff63
Financi Financi <Dima@DIMA-PC>
2018-02-03 22:05:52 +0200

    TableAdapterFactory and test

---
+++ SOPB.DAL/LoadData/TableAdapterFactory.cs
@@ -0,0 +1,54 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using SOPB.Accounting.DAL.TableAdapters;
+using SOPB.Accounting.DAL.TableAdapters.CustomerTableAdapter;
+using SOPB.Accounting.DAL.TableAdapters.Glossary;
+
+namespace SOPB.Accounting.DAL.LoadData
+{
+    public static class TableAdapterFactory
+    {
+        public static BaseTableAdapter AdapterFactory(string tableName)
+        {
+            switch (tableName.ToUpper())
+            {
+                case "CUSTOMER":
+                    return new CustomerTableAdapter();
+                case "ADDRESS":
+                    return new AddressTableAdapter();
+                case "REGISTER":
+                    return new RegisterTableAdapter();
+                case "APPPTPR":
+                    return new ApppTprTableAdapter();
+                case "ADMINDIVISION":
+                    return new AdminDivisionTableAdapter();
+                case "TYPESTREET":
+                    return new TypeStreetTableAdapter();
+                case "LAND":
+                    return new LandTableAdapter();
+                case "WHYDEREGISTER":
+                    return new WhyDeRegisterTableAdapter();
+                case "REGISTERTYPE":
+                    return new RegisterTypeTableAdapter();
+                case "CHIPERRECEPT":
+                    return new ChiperReceptTableAdapter();
+                case "DISABILITYGROUP":
+                    return new DisabilityGroupTableAdapter();
+                case "BENEFITSCATEGORY":
+                    return new BenefitsCategoryTableAdapter();
+                case "INVALID":
+                    return new InvalidTableAdapter();
+                case "INVALIDBENEFITSCATEGORY":
+                    return new InvalidBenefitsCategoryTableAdapter();
+                case "GENDER":
+                    return new GenderTableAdapter();
+                default:
+                    return null;
+
+            }
+        }
+    }
+}
---
+++ SOPB.DALUnitTest/TableAdapters/LoadDataTest/TableAdapterFactoryTest.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+using SOPB.Accounting.DAL.LoadData;
+
+namespace SOPB.DALUnitTest.TableAdapters.LoadDataTest
+{
+    [TestClass]
+    public class TableAdapterFactoryTest
+    {
+        [TestMethod]
+        public void NullTableAdapterTest()
+        {
+            var obj = TableAdapterFactory.AdapterFactory("Abracadabra");
+            Assert.IsNull(obj);
+        }
+        [TestMethod]
+        public void GetTableAdapterTest()
+        {
+            var obj = TableAdapterFactory.AdapterFactory("CuSToMer");
+            Assert.IsNotNull(obj);
+        }
+    }
+}
